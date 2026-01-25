7f512a22a86a7b9ad3fdece5b8cb12e6692d28ca
Jonathan Siqueira <jonathansiqueiraslv@gmail.com>
2026-01-25 16:20:21 -0300

    correção menu e titulo no mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -57,7 +57,7 @@ export default function RootLayout({
       >
         <ThemeProvider>
           <GlobalToastProvider>
-            <div className="flex min-h-screen">
+            <div className="flex flex-col md:flex-row min-h-screen">
               <Sidebar />
               <MobileNav />
               <main className="flex-1 p-8 md:p-12 md:pl-80 transition-all duration-300">
